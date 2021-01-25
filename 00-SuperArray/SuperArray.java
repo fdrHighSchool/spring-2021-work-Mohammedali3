7205cb6531c0c0a5948b0ae7d29d6454c81661b5
Mohammed Ali <mohammeda258@nycstudents.net>
2021-01-25 14:56:48 -0500

    tried to add add method

--- 00-SuperArray/SuperArray.java
+++ 00-SuperArray/SuperArray.java
@@ -2,7 +2,21 @@ import java.util.Arrays;
 
 public class SuperArray {
   // instance variables
-  private String[] arr;
-  private int numElements = 0;
-  
-}
+  private int[] array;
+  private int count;
+
+  public SuperArray(){
+    this.array = new int[10];
+  }//end constructor
+
+  public void add(int val){
+    for(int i = 0; i < this.array.length; i++ ){
+      if(array[i] == 0){
+        
+      }
+
+
+    }
+  }//end add
+
+}//end SuperArray(main)
