f6446b626df9ea3d3ff24c1cc96e206e0e039f36
Mohammed Ali <mohammeda258@nycstudents.net>
2021-01-28 08:44:46 -0500

    Added more code

---
+++ 00-SuperArray/Driver.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.until.Arrays;
+
+public class Driver.java {
+  public static void main(String[] args) {
+    array = 
+  }
+}
--- 00-SuperArray/SuperArray.java
+++ 00-SuperArray/SuperArray.java
@@ -9,14 +9,34 @@ public class SuperArray {
     this.array = new int[10];
   }//end constructor
 
+  public SuperArray(){
+    this.array = new int[Length];
+  }//end constructor
+
+ //empty
+ //there is Space
+ //array is full
   public void add(int val){
+    int Length = this.array.length;
+
     for(int i = 0; i < this.array.length; i++ ){
-      if(array[i] == 0){
-        
+      if(array[i] == null){
+        this.array[i] = val;
+        return;
       }
 
+      int array[] = new int[Length + 1];
 
     }
   }//end add
+
+  public boolean isEmpty(int val){
+    for(int i = 0; i < this.array.length; i++ ){
+      if(this.array[i] != null){
+        return false;
+      }
+    }
+    return true;
+  }
 
 }//end SuperArray(main)
